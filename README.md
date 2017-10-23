d425f0942e5034e684e8068e5d92bcbef6d152cc
Fabrice Delahaij <info@extreemhost.nl>
2017-10-23 22:12:11 +0200

    Version 1.0
    Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# JORTTBV-PHP-API
+# Jortt API client for PHP
+PHP WEB API JSON for [Jortt B.V](https://jortt.nl)
+
+[![Floobits Status](https://floobits.com/Extreemhost/JORTTBV-PHP-WEB-API.svg)](https://floobits.com/Extreemhost/JORTTBV-PHP-WEB-API/redirect)
+
+## Requirements ##
+To use the Jortt API client, the following things are required:
+
++ Visit website for free [Jortt account](https://app.jortt.nl/aanmelden/gratis)
++ Create a new [profile](https://app.jortt.nl/profile/api) to generate API keys
++ PHP 5.2 or 5.5
++ PHP cURL extension
+
+## Getting started ##
+
+```php
+require_once('class.jorttbv.php'); 
+
+define('APPID', 'DEMO');
+define('APPTOKEN', '000-000-000');
+
+$api = new JorttBV_Client(APPID, APPTOKEN);
+$data = [
+	'customer' => [
+		'company_name' => '',
+		'attn' => '',
+		"email" => '',
+		'extra_information' => '',
+		'invoice_language' => NL,
+		'address' =>[
+		        'street' => '',
+			'postal_code' => '',
+        		'city' => '',
+        		'country_code' => NL
+		]
+	]
+];
+$api->request($data, 'customers');
+```
+
+## API documentation ##
+If you wish to learn more about our API, please visit the [API Documentation](https://app.jortt.nl/api-documentatie).
+
+## License ##
+
+[BSD (Berkeley Software Distribution) License](https://opensource.org/licenses/bsd-license.php). Copyright (c) 2017, Extreemhost.
+
+## Support ##
+ Contact: [www.extreemhost.nl](https://extreemhost.nl) ? info@extreemhost ? +31 316-23 40 40
---
+++ class.jorttbv.php
@@ -0,0 +1,45 @@
+<?php
+	class JorttBV_Client	{
+		protected $APIURL = 'https://app.jortt.nl/api/';
+
+		public function __construct($appname, $apptoken){
+			$this->appname = $appname;
+			$this->apptoken = $apptoken;
+		}
+
+		public function request($string, $select) {
+			$request = curl_init();
+			$headers =  array(
+				'Content-Type: application/json',
+				'Accept: application/json',
+				'Authorization: Basic '.base64_encode($this->appname.':'.$this->apptoken)
+			);
+			$config = array(
+				'APPNAME' => $this->appname,
+				'APITOKEN' => $this->apptoken,
+				'APIURL' => $this->APIURL
+			);
+			$command = json_encode($string);
+			curl_setopt($request, CURLOPT_URL, $config['APIURL'].$select);
+			curl_setopt($request, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($request, CURLOPT_POST, true);
+			curl_setopt($request, CURLOPT_POSTFIELDS, $command);
+			curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($request, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($request, CURLOPT_HEADER, false);	
+			curl_setopt($request, CURLOPT_USERPWD, base64_encode($config['APPNAME'].':'.$config['APITOKEN']));
+			$result = curl_exec($request);
+			if ($result == FALSE) {
+				print('<p>Curl failed: '.curl_error($request).'</p>');
+			}
+			// Close connection
+			curl_close($request);
+			// Decode JSON response.
+			$response = json_decode($result, true);
+			//
+			//return $response;
+			print $result;
+		}
+	}
